35e6b440fc344843d8ebb195f6870f7676464c1d
hushichang <hushichang@stardust.ai>
2018-09-24 11:00:28 +0800

    定制显示icon

--- app/models.py
+++ app/models.py
@@ -1,5 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_admin.contrib.sqla import ModelView
+from flask_admin.form import rules
+from wtforms.validators import required
+
 db = SQLAlchemy()
+
+
+class RoleModelView(ModelView):
+    pass
+    # # 这个是干什么的呢, 擦，看着没什么用啊
+    # form_ajax_refs = {
+    #     'users': {
+    #         'fields': ['name', 'email'],
+    #         'page_size': 10
+    #     }
+    # }
 
 
 class Role(db.Model):
@@ -13,6 +28,54 @@ class Role(db.Model):
     # 相当于__str__方法。
     def __repr__(self):
         return "Role: %s %s" % (self.id, self.name)
+
+
+class UserModelView(ModelView):
+    list_template = 'custom_list.html'
+    # # 目前没看出来这玩意儿有什么用
+    # # 这玩意儿到底有什么用呢，我日
+    # # 这个是在创建的时候显示的，会有竖着的三栏， 但是这个创建的Foobar后面是空着的
+    # form_create_rules = ('name', rules.Text('Foobar'), 'email', 'password')
+
+    # # 下面这个得琢磨琢磨是用来做什么的了
+    # form_extra_fields = ('hehehehehe')
+
+    # 这个又是什么东西呢？
+    create_modal = True
+    edit_modal = True
+
+    # 这个里面所谓的表格都指的是，编辑和创建的时候的表格
+    # 这个相当于在床架你的时候，给name这个值进行有限制的选择，会有一个下拉栏的
+    # 但是为什么是元组呢？ 难道是一个映射吗
+    # 显示的是前面，设置的是后面
+    form_choices = {
+        'password': [
+            ('1', '111111'),
+            ('2', '222222'),
+            ('3', '333333'),
+            ('4', '444444'),
+            ('5', '555555')
+        ]
+    }
+
+    # 这个东西又是什么东西呢？
+    # 在创建和编辑的时候，name字段，显示的事First Name,但是呢，这个必须存在
+    form_args = {
+        'name': {
+            'label': 'First Name',
+            'validators': [required()]
+        }
+    }
+
+    # 这个是什么用的呢, 真的没看懂这个到底是干嘛的
+    # 这个style中似乎是在编辑和创建的时候，输入的name或者显示的name为红色
+    # 那么这个rows的参数用来干什么呢
+    form_widget_args = {
+        'name': {
+            'rows': 10,
+            'style': 'color: red'
+        }
+    }
 
 
 class User(db.Model):
--- main.py
+++ main.py
@@ -4,15 +4,18 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from app.models import User, Role
 from app.models import db
+from app.models import UserModelView, RoleModelView
 app = Flask(__name__)
 # mac下的密码是 hscxrzs1st
 # ubuntu下的密码是 669193
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:hscxrzs1st@localhost/first_flask'
+# 这个不设置的话，会出现不可以修改数据库的内容
+app.config['SECRET_KEY'] = 'Mr.hu is handsome boy'
 # 初始化数据库
 db.init_app(app)
 admin = Admin(app, name="Mr.Hu's test")
-admin.add_view(ModelView(User, db.session))
-admin.add_view(ModelView(Role, db.session))
+admin.add_view(UserModelView(User, db.session))
+admin.add_view(RoleModelView(Role, db.session))
 
 
 @app.route('/')
